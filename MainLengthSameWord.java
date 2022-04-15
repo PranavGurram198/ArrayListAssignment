73daa4657686feb2f9da92bef7f09e75913bb24e
PranavGurram198 <101916625+PranavGurram198@users.noreply.github.com>
2022-04-15 23:37:19 +0530

    Add files via upload

---
+++ MainLengthSameWord.java
@@ -0,0 +1,14 @@
+
+
+import java.util.Scanner;
+
+public class MainLengthSameWord {
+
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(UserMainCode_LengthSameWord.empdis(s));
+ 
+}
+
+}
